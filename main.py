a34eef4f9ffa8c985d60144644cf9755b857ea67
Rajat Mishra <rajat95195@gmail.com>
2021-06-08 15:43:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,33 @@ def Virus_test(strchar):
    if N>0 and N<11:
         for i in range(0,N):
          sample.insert(i,input())
-        print(sample) 
-           
-   else:
+        for j in range(0,N):
+            temp=sample[j]
+            m=0
+            for k in range(0,len(temp)):
+                c=temp[k]
+                
+                temp1=0
+                count=0
+                for l in range(0,len(strchar)):
+                    if c==strchar[l]:
+                        temp1=l
+                        break
+                        
+                    count=count+1
+                
+                if count==len(strchar):
+                    print("NEGATIVE")
+                    break
+                if temp1>m:
+                    m=temp1
+                if temp1<m:
+                    print("NEGATIVE")
+                    break
+                if k==len(temp)-1:
+                    print("POSITIVE")
+   else:        
        return "invalid input"
-
+   return ""
 print(Virus_test(input()))        
     
